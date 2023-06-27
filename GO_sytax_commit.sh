c391f2f4b8459e2275ae6f6bfb62b1e835446af6
Just161 <109623921+just1984@users.noreply.github.com>
2023-06-27 09:40:25 +0200

    Add files via upload

---
+++ GO_sytax_commit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x /pfad/zu/deinem/skript/commit_script.sh
+/pfad/zu/deinem/skript/commit_script.sh
---
+++ xx_sytax_commit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /pfad/zu/deinem/projekt
+
+while true; do
+    hour=$(date +%H)
+    minute=$(date +%M)
+
+    # Führe die Befehle aus, wenn es 9 Uhr morgens ist
+    if [ $hour -eq 9 ] && [ $minute -lt 10 ]; then
+        git add .
+        git commit -m "morgens"
+        git push
+        if [ $? -ne 0 ]; then
+            echo "Ein Fehler ist aufgetreten bei der Ausführung des morgendlichen git-Befehls"
+            exit 1
+        fi
+    fi
+
+    # Führe die Befehle aus, wenn es 16 Uhr nachmittags ist
+    if [ $hour -eq 16 ] && [ $minute -lt 10 ]; then
+        git add .
+        git commit -m "nachmittags"
+        git push
+        if [ $? -ne 0 ]; then
+            echo "Ein Fehler ist aufgetreten bei der Ausführung des nachmittäglichen git-Befehls"
+            exit 1
+        fi
+    fi
+
+    # Warte 10 Minuten
+    sleep 600
+done
