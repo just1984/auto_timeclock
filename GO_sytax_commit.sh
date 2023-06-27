441a88bb6e55a52cbc2f8475d4ba4aa29bd75128
Just161 <109623921+just1984@users.noreply.github.com>
2023-06-27 10:07:15 +0200

    Add files via upload

--- GO_sytax_commit.sh
+++ GO_sytax_commit.sh
@@ -3,5 +3,5 @@
 # Lese den Pfad aus der Konfigurationsdatei
 script_path=$(cat xx_pfadzumprojekt.txt)
 
-chmod +x $script_path/commit_script.sh
-$script_path/commit_script.sh
+chmod +x $script_path/xx_sytax_commit.sh
+$script_path/xx_sytax_commit.sh
