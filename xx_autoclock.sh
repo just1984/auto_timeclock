104106e4434124fcadf21d84fe688c82fff5c4bf
Just161 <109623921+just1984@users.noreply.github.com>
2023-07-01 09:02:52 +0200

    Update xx_autoclock.sh

--- xx_autoclock.sh
+++ xx_autoclock.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 
-if [ ! -f xx_pfadzumprojekt.txt ]; then
-    echo "Error: Configuration file xx_pfadzumprojekt.txt not found."
-    exit 1
-fi
+function read_project_path() {
+    if [ ! -f xx_pfadzumprojekt.txt ]; then
+        echo "Error: Configuration file xx_pfadzumprojekt.txt not found."
+        exit 1
+    fi
 
-project_path=$(cat xx_pfadzumprojekt.txt)
+    local project_path=$(cat xx_pfadzumprojekt.txt)
 
-if [ -z "$project_path" ]; then
-    echo "Error: No path specified in xx_pfadzumprojekt.txt."
-    exit 1
-fi
+    if [ -z "$project_path" ]; then
+        echo "Error: No path specified in xx_pfadzumprojekt.txt."
+        exit 1
+    fi
 
-if [ ! -d "$project_path" ]; then
-    echo "Error: Directory $project_path does not exist."
-    exit 1
-fi
+    if [ ! -d "$project_path" ]; then
+        echo "Error: Directory $project_path does not exist."
+        exit 1
+    fi
 
-cd $project_path
+    echo $project_path
+}
 
-sudo chmod -R 777 .git
+function check_git_repo() {
+    local project_path=$1
 
-if [ ! -d ".git" ]; then
-    echo "Error: Directory $project_path is not a Git repository."
-    exit 1
-fi
+    if [ ! -d "$project_path/.git" ]; then
+        echo "Error: Directory $project_path is not a Git repository."
+        exit 1
+    fi
+}
 
-laufnummer=0
+function execute_git_commands() {
+    local laufnummer=$1
 
-# Configure the wait time in seconds between commits.
-# You can uncomment one of the following options:
-
-# Option 1: Commit every hour
-# secs=3600
-
-# Option 2: Commit every 2 hours
-# secs=$((2 * 3600))
-
-# Option 3: Commit every 6 hours
-# secs=$((6 * 3600))
-
-# Option 4: Commit every 12 hours
-# secs=$((12 * 3600))
-
-# Option 5: Random commit time within 120 minutes
-secs=$((RANDOM % 7200))
-
-while true; do
-    echo "Executing git commands.."
     git pull
     git add .
     let "laufnummer++"
     git commit --allow-empty -m "Commit at $(date), sequence number $laufnummer"
     git push
+
     if [ $? -ne 0 ]; then
         echo "An error occurred when executing the git command"
         exit 1
     fi
+
+    echo $laufnummer
+}
+
+project_path=$(read_project_path)
+check_git_repo $project_path
+cd $project_path
+sudo chmod -R 777 .git
+laufnummer=0
+secs=$((RANDOM % 7200))
+
+while true; do
+    echo "Executing git commands.."
+    laufnummer=$(execute_git_commands $laufnummer)
 
     while [ $secs -gt 0 ]; do
         echo -ne "Waiting until next commit: $secs\033[0K\r"
